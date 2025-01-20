71a8758a92e8f588e68e992253b210bd72e2bfe8
jaroslav380 <jaroslav.romashchuk@gepardec.com>
2025-01-20 14:49:07 +0100

    add User CRUD - operations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,12 @@ import {GameComponent} from './game/game.component';
 import {GameCreateFormComponent} from './game-create-form/game-create-form.component';
 import {GameDetailComponent} from './game-detail/game-detail.component';
 import {GameEditFormComponent} from './game-edit-form/game-edit-form.component';
+import {UserListComponent} from './user-list/user-list.component';
+import {UserDetailsComponent} from './user-details/user-details.component';
+import {CreateUserComponent} from './create-user/create-user.component';
+import {HomeScreenComponent} from './home-screen/home-screen.component';
+import {RankListComponent} from './rank-list/rank-list.component';
+import {UpdateUserComponent} from './update-user/update-user.component';
 
 export const routes: Routes = [
   { path: '', component: HomeScreenComponent, title: 'Home' },
