8e56c565b8026df08a91dc3557751e9e9723d95b
Aleksandar Zivkovic <aleksandar.zivkovic@gepardec.com>
2025-02-05 14:58:01 +0100

    add match history to gamedetail

--- src/app/match.service.ts
+++ src/app/match.service.ts
@@ -1,7 +1,7 @@
 import {inject, Injectable} from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
+import {HttpClient, HttpHeaderResponse, HttpParams, HttpResponse} from '@angular/common/http';
 import {CreateMatch, Match} from './match';
-import {config, Observable} from 'rxjs';
+import {config, map, Observable} from 'rxjs';
 import {ConfigService} from './shared/config.service';
 
 @Injectable({
@@ -20,9 +20,35 @@ export class MatchService {
     return this.http.post<Match>(this.configService.getBackendUrlWithContext(`${this.url}`), match);
   }
 
-  getAllMatchesFromGame(gameToken: string) {
+  getAllMatchesFromGame(gameToken: string): Observable<Match[]> {
     return this.http.get<Match[]>(
       this.configService.getBackendUrlWithContext(this.url),
       { params: new HttpParams().set("gameToken", gameToken) });
+  }
+
+  getXAmountOfLastMatches(gameToken: string, amount: number): Observable<Match[]> {
+    return this.http.get<Match[]>(
+      this.configService.getBackendUrlWithContext(this.url),
+      { params: new HttpParams().set("gameToken", gameToken).set("pageSize", amount).set("pageNumber", "1") });
+  }
+
+  getXAmountOfLastMatchesForUser(gameToken: string, amount: number, userToken: string): Observable<Match[]> {
+    return this.http.get<Match[]>(
+      this.configService.getBackendUrlWithContext(this.url),
+      { params: new HttpParams().set("gameToken", gameToken).set("pageSize", amount).set("pageNumber", "1").set("userToken", userToken) });
+  }
+
+  getMatchCount(gameToken: string): Observable<number> {
+    return this.http.head(
+      this.configService.getBackendUrlWithContext(this.url),
+      {observe: 'response', params: new HttpParams().set("gameToken", gameToken)})
+        .pipe(
+          map((event: any) => {
+            if (event instanceof HttpResponse) {
+              return Number(event.headers.get('X-Total-Count'));
+            }
+            return 0;
+          }),
+        );
   }
 }
