9bd35dd6c00b537360693043f01fbe8a5e3b6489
Aleksandar Zivkovic <aleksandar.zivkovic@gepardec.com>
2025-02-18 15:28:07 +0100

    add pwa support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptors} from '@angular/common/http'
 import { authInterceptor } from './auth.interceptor';
@@ -7,13 +7,26 @@ import { authInterceptor } from './auth.interceptor';
 import { routes } from './app.routes';
 import {NotificationInterceptor} from './notification.interceptor';
 import {NotificationService} from './notification.service';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
     provideHttpClient(
       withInterceptors([authInterceptor]),
       withInterceptors([NotificationInterceptor]),
-    ), NotificationService
+    ), NotificationService, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
 
   ]
 };
