8d8c61cdd4b3c50d02c21ecfa2e1a486ae9f35e8
jaroslav380 <jaroslav.romashchuk@gepardec.com>
2025-02-28 13:50:45 +0100

    add create match-button for all screens & improve some heights

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router, RouterOutlet} from '@angular/router';
 import {UserListComponent} from './user-list/user-list.component';
 import {RouterModule} from '@angular/router';
 import {HomeHeaderComponent} from './home-header/home-header.component';
 import {NotificationComponent} from './notification/notification.component';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, HomeHeaderComponent, NotificationComponent],
+  imports: [RouterOutlet, RouterModule, HomeHeaderComponent, NotificationComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent{
   title = 'gepardec-gamertrack-frontend';
+
+  constructor(private router: Router) {}
+
+  isNewMatchRoute(): boolean {
+    return this.router.url.includes('/new');
+  }
 }
+
+
