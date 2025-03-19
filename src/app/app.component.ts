5f0f979c704cbcddf2f004fd6b325d43968871f4
jaroslav380 <jaroslav.romashchuk@gepardec.com>
2025-03-19 16:02:03 +0100

    change visibility of fixedCreateButton only if logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {Router, RouterOutlet} from '@angular/router';
 import {UserListComponent} from './user/ui/user-list/user-list.component';
 import {RouterModule} from '@angular/router';
 import {HomeHeaderComponent} from './core/ui/home-header/home-header.component';
 import {NotificationComponent} from './core/ui/notification/notification.component';
 import {NgIf} from '@angular/common';
+import {AuthService} from './core/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -15,9 +16,14 @@ import {NgIf} from '@angular/common';
 export class AppComponent {
   title = 'gepardec-gamertrack-frontend';
 
+  authService = inject(AuthService);
+
   constructor(private router: Router) {}
 
   isNewMatchRoute(): boolean {
     return this.router.url.includes('/new');
+  }
+  isAuthorized() {
+    return this.authService.isAuthenticated();
   }
 }
